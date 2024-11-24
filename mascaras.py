000facf6b5219679eb8ff1d3b2c574bc72ef86d6
DiegoNavaArsola <52011366+DiegoNavaArsola@users.noreply.github.com>
2024-11-24 15:19:23 -0600

    Eliminación del fondo. Segmentación por cierre

--- mascaras.py
+++ mascaras.py
@@ -1,6 +1,28 @@
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+def remove_background(img, output_path):
+
+    imagen_filtrada = cv2.bilateralFilter(img, d=25, sigmaColor=100, sigmaSpace=100)
+
+    # Aplicar un umbral adaptativo para separar fondo y objeto
+    _, binary = cv2.threshold(imagen_filtrada, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Invertir la máscara binaria
+    #binary_inv = cv2.bitwise_not(binary)
+    binary_inv = binary
+
+    # Aplicar operaciones morfológicas para eliminar ruido
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    binary_cleaned = cv2.morphologyEx(binary_inv, cv2.MORPH_CLOSE, kernel)
+
+    # Aplicar la máscara limpia sobre la imagen original
+    foreground = cv2.bitwise_and(img, img, mask=binary_cleaned)
+
+
+    return foreground
 
 def erotion_dilation(img, kernel, threshold):
 
@@ -45,17 +67,44 @@ def paste_mask(img_original, img_mask):
 
 if __name__ == "__main__":
 
-    input_dir_png = "Estudio_1_png"
-
-    # Cargar la imagen
-    imagen = cv2.imread(input_dir_png + '/BOULLOSA-MADRAZO-ANTONIO.transformed150.png',
-                        cv2.IMREAD_GRAYSCALE)  # Leer en escala de grises
+    kernel_size = 7
 
     # Definir una máscara de suavizado
-    kernel = (1 / (11 ** 2)) * np.ones((11, 11), np.uint8)
+    kernel = (1 / (kernel_size ** 2)) * np.ones((kernel_size, kernel_size), np.uint8)
     # kernel = cv2.getGaussianKernel(11,1)
 
-    umbral = 157
+    umbral = 155
+
+    input_dir_png = "Estudio_1_png"
+    output_dir_background = "Estudio_1_png/background_remove"
+    output_dir_mask = "Estudio_1_png/mascaras"
+
+    for file in os.listdir(input_dir_png):
+        if file.lower().endswith('.png'):
+
+            # Cargar la imagen
+            img = cv2.imread(input_dir_png+"/"+file, cv2.IMREAD_GRAYSCALE)
+
+            # Quitar fondo
+            img_foreground = remove_background(img,None)
+
+            output_filename_bck = os.path.splitext(file)[0] + '_foreground.png'
+            output_filepath_bck = os.path.join(output_dir_background, output_filename_bck)
+            cv2.imwrite(output_filepath_bck, img_foreground)
+
+            # Sección de interés
+            img_section = opening_closing(img_foreground,umbral,kernel_size)
+
+            img_mask = paste_mask(img_foreground, img_section)
+
+            # Guardar la imagen en formato PNG
+            output_filename = os.path.splitext(file)[0] + '_region.png'
+            output_filepath = os.path.join(output_dir_mask, output_filename)
+            cv2.imwrite(output_filepath,img_mask)
+            print(f"Imagen {output_filename} -> Correcto")
+
+
+    """imagen = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
 
     erosion, dilatacion = erotion_dilation(imagen, kernel, umbral)
 
@@ -77,4 +126,4 @@ if __name__ == "__main__":
     cv2.imshow("Imagen con cierre", imagen_con_cierre)
 
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows()"""
